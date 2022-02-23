c938dc25dda0af10f45143e07d28bd827d3304ba
sodara <christiansodara00@gmail.com>
2022-02-23 01:47:11 +0000

    projet rqvpolice

---
+++ cms/admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2021 SODARA </b> All rights reserved.
+		</div>
+	</div>
---
+++ cms/users/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+              2021 -SODARA_DEV
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
